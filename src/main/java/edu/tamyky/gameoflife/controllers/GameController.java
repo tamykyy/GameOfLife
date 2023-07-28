7dbb746166d05aa83b4da0eb2433d183be9109e0
Mykyta Vykhodtsev <deriel20021903@gmail.com>
2023-07-28 15:35:20 +0300

    add world moving

--- src/main/java/edu/tamyky/gameoflife/controllers/GameController.java
+++ src/main/java/edu/tamyky/gameoflife/controllers/GameController.java
@@ -3,14 +3,19 @@ package edu.tamyky.gameoflife.controllers;
 import edu.tamyky.gameoflife.game.Game;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Cursor;
 import javafx.scene.Node;
 import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseButton;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.StackPane;
 import javafx.scene.shape.Rectangle;
 
 import java.net.URL;
+import java.util.Map;
 import java.util.ResourceBundle;
+import java.util.Stack;
 
 public class GameController implements Initializable {
 
@@ -19,9 +24,13 @@ public class GameController implements Initializable {
     @FXML
     private GridPane gridPane;
 
+    private Map.Entry<Double, Double> clickCoordinate;
+    private Map.Entry<Double, Double> gridInitialPosition;
+
     public void cellClicked(MouseEvent event) {
         Node clickedNode = event.getPickResult().getIntersectedNode();
-        if (clickedNode != gridPane) {
+        MouseButton button = event.getButton();
+        if (clickedNode != gridPane && button == MouseButton.PRIMARY) {
             game.updateCell((Rectangle) clickedNode);
         }
     }
@@ -30,6 +39,25 @@ public class GameController implements Initializable {
         switch (event.getCode()) {
             case SPACE -> game.doIteration();
             default -> System.out.println("just click");
+        }
+    }
+
+    public void onMousePressed(MouseEvent event) {
+        if (event.getButton() == MouseButton.MIDDLE) {
+            clickCoordinate = Map.entry(event.getSceneX(), event.getSceneY());
+            gridInitialPosition = Map.entry(gridPane.getTranslateX(), gridPane.getTranslateY());
+        }
+    }
+
+    public void onMouseDragged(MouseEvent event) {
+        gridPane.setCursor(Cursor.CLOSED_HAND);
+        MouseButton button = event.getButton();
+        if (button == MouseButton.MIDDLE) {
+            System.out.println(gridPane.getScaleZ());
+
+            gridPane.setScaleZ(gridPane.getScaleZ() + 10);
+            gridPane.setTranslateX(gridInitialPosition.getKey() - (clickCoordinate.getKey() - event.getSceneX()));
+            gridPane.setTranslateY(gridInitialPosition.getValue() - (clickCoordinate.getValue() - event.getSceneY()));
         }
     }
 
